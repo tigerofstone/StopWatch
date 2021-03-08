ce614abc12ea0281e4df053d3f039d16c2db5b96
tiger.ma <tiger.ma@bogon>
2021-03-08 15:33:53 +0800

    2021-03-08

--- src/StopWatch/StopWatchC.java
+++ src/StopWatch/StopWatchC.java
@@ -6,8 +6,8 @@ import javax.swing.*;
 
 public class StopWatchC extends JApplet
 {
-    /**
-     *
+    /*
+     
      */
     private static final long serialVersionUID = 1L;
     private boolean isStandalone = false;
